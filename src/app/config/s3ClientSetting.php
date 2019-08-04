59fde5f3242f18a7469873355bcac68a7a327bd2
ryota <kryota.gin@gmail.com>
2019-08-04 16:58:39 +0900

    S3のCredentialに任意の設定オプションを追加

--- src/app/config/s3ClientSetting.php
+++ src/app/config/s3ClientSetting.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * Notice.
+ * In general, please operate with ec2.
+ * Create a role to give access to s3 and attach that role to ec2.
+ * If you do not use ec2, add the access key and secret key to the array value defined below
+ * However, please be careful about leakage of such information
+ */
 
-// S3 StreamWrapperProtocol
-define('S3_PROTOCOL', 's3://');
-
+/* When operating with docker. Or when not using ec2
+ *-----------------------------------------------------*/
+define('ACCESS_KEY', 'Access key you got at AWS IAM');
+define('SECRET_KEY', 'Secret key you got at AWS IAM');
 //S3ClientSetting
 define('S3_SET_OPTIONS', [
     'version' => 'latest',
     'region'  => 'ap-northeast-1',
+    'credentials' => [
+        'key' => ACCESS_KEY,
+        'secret' => SECRET_KEY,
+    ]
 ]);
+
+/* When operating with ec2
+ *-----------------------------------------------------*/
+//S3ClientSetting
+// define('S3_SET_OPTIONS', [
+//     'version' => 'latest',
+//     'region'  => 'ap-northeast-1',
+// ]);
+
+// S3 StreamWrapperProtocol
+define('S3_PROTOCOL', 's3://');
