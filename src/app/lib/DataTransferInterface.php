4f228732d21f7d95766a7b0e3ae0770831212221
ryota <kryota.gin@gmail.com>
2019-08-14 00:49:29 +0900

    Interface追加

---
+++ src/app/lib/DataTransferInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace App;
+
+interface DataTransferInterface
+{
+    public function setProperties(array $properties);
+
+    
+
+    public function All();
+
+    
+
+    public function getValues(array $propnames);
+}
