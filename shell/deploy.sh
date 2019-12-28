233808b145ba5d374270bb024c741bbc37678cd0
ryotakoyama <kryota.gin@gmail.com>
2019-12-28 16:01:00 +0900

    deployのjobでCircleCiから叩くshell

---
+++ shell/deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd /var/www/html/
+
+git fetch origin
+git reset --hard origin/master
+
+chmod 666 /var/www/html/src/app/auth/*
+
+wget mysite/app/lib/OpcacheReset.php -q -O
+
+bash path to required.sh
+
+composer install --ignore-platform-reqs --no-scripts
---
+++ shell/required.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# You need to execute "git config core.sparsecheckout true" after git init
+{
+  echo "/src"
+  echo "composer.json"
+  echo "composer.lock"
+  echo "package-lock.json"
+  echo "package.json"
+  echo "webpack.config.js"
+} > .git/info/sparse-checkout
+git read-tree -m -u HEAD
