d25fec3b52960fb3a100c6a4d7091ce92993c29b
ryotakoyama <kryota.gin@gmail.com>
2019-12-30 01:31:03 +0900

    jsとsassのbundle結果をmodel別にoutput

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,72 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const assets = path.join(__dirname, "./src/app/assets/");
+const dist = assets + "dist/";
+const froms = {
+  js: {
+    account: assets + "js/account.js",
+    main: assets + "js/main.js"
+  },
+  css: {
+    account: assets + "styles/account/account.scss",
+    main: assets + "styles/main/main.scss"
+  }
+};
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-      account : './src/app/asset/js/account.js',
-      main : './src/app/asset/js/main.js',
+    account: froms.js.account,
+    main: froms.js.main,
+    "account.css": froms.css.account,
+    "main.css": froms.css.main
   },
   output: {
-      filename: "[name].js",
-      path: path.join(__dirname, './src/app/asset/js/dist')
+    filename: "js/[name].js",
+    path: dist
   },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin({
+      extensions: ["scss", "css"]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name]"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin({
+      extensions: ["scss", "css"]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name]"
+    })
+  ],
   resolve: {
     alias: {
-      'jui': 'jquery-ui/ui',
+      jui: "jquery-ui/ui"
     }
   },
+<<<<<<< HEAD
+=======
+  cache: true
+>>>>>>> 34e0d3e3... jsとsassのbundle結果をmodel別にoutput
 };
