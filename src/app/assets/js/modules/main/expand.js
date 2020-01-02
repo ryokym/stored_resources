5bc6649eb62700ab182b74da9e39dfa69a73ce6c
ryotakoyama <kryota.gin@gmail.com>
2020-01-02 11:08:30 +0900

    脱jQuery 折りたたみの実装をwebAnimationsApiに変更

--- src/app/assets/js/modules/main/expand.js
+++ src/app/assets/js/modules/main/expand.js
@@ -1,50 +1,45 @@
-import $ from "jquery";
 import common from "../common/common.js";
+import "web-animations-js/web-animations-next.min.js";
 
 /* expand
 -------------------------------------------------------*/
+const state = {
+  width: 0
+};
 
 export default function() {
-  const expand = {
-    stateWidth: 0,
-    leftgap: 42
-  };
-
   return (function() {
-    const $column = $("#column"),
-      $display = $("#display");
+    const column = document.getElementById("column");
+    const display = document.getElementById("display");
+
+    const collapse = function(distance, isHide = false, isBack = false) {
+      const elm = this;
+      const width = distance ? distance : elm.offsetWidth;
+      const to = isBack ? width : -width;
+      this.animate(
+        [{ left: "0px" }, { left: to + "px" }],
+        500
+      ).onfinish = function() {
+        const prop = isHide ? "none" : "flex";
+        elm.style.display = prop;
+      };
+      if (!distance) {
+        return width;
+      }
+    };
 
     if (main.isview && !common.ismode("remove")) {
       common.rotate(common.mode, ["expand", "change"], function(nextmode) {
         common.setmode(nextmode);
       });
       if (common.ismode("change")) {
-        $column.show();
-        $display.css({
-          "margin-left": 0,
-          right: expand.stateWidth - expand.leftgap
-        });
-        $display.animate({ right: 0 });
-        $column.animate({ right: 0 });
+        collapse.call(display, state.width, false, true);
+        collapse.call(column, state.width, false, true);
       }
+
       if (common.ismode("expand")) {
-        expand.stateWidth = $column.outerWidth();
-        var maxWidth = $(".container").innerWidth();
-        $column.animate({
-          right: expand.stateWidth
-        });
-        $display.animate(
-          { right: expand.stateWidth - expand.leftgap },
-          {
-            complete: function() {
-              $display.css({
-                "margin-left": expand.leftgap,
-                right: 0
-              });
-              $column.hide();
-            }
-          }
-        );
+        state.width = collapse.call(column, "", true);
+        collapse.call(display, state.width);
       }
     }
   })();
