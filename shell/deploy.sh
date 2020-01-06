dd7929e0b26dd9fe8a12f8ac4113c49e56d803ae
ryotakoyama <kryota.gin@gmail.com>
2020-01-06 22:36:39 +0900

    ユーザ管理ファイルpermission変更用スクリプト削除

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -5,8 +5,6 @@ cd /var/www/html/
 git fetch origin
 git reset --hard origin/master
 
-chmod 666 /var/www/html/src/app/auth/*
-
 wget mysite/app/lib/OpcacheReset.php -q -O
 
 bash path to required.sh
