52840982eedad1cad1d5ec4637291dc3b5c3b4d0
ryotakoyama <kryota.gin@gmail.com>
2020-01-08 01:40:41 +0900

    モードの切替え機能だけ実装

--- src/views/account.jsx
+++ src/views/account.jsx
@@ -1,138 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import common from "./modules/common/common.js";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Account from "./containers/Account";
+import reducer from "./reducers";
 
-class Account extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: "",
-      context: "",
-      prev: "sign_up",
-      prevHover: "sign_up_hover",
-      username: "",
-      password: "",
-      bucket: "",
-      bucketkey: "",
-      bucketval: ""
-    };
-    this.modes = ["enter", "create"];
-    this.forward = "/";
-    this.execpath = "/api/account.php";
-    this.done = response => {
-      let isFinish = false;
-      if (typeof this[response] == "function") {
-        isFinish = true;
-        this[response](isFinish);
-      } else {
-        alert(response);
-      }
-    };
-  }
-  componentDidMount() {
-    this.enter();
-  }
+const store = createStore(reducer);
 
-  enter(isfinish) {
-    if (isfinish) {
-      location.href = this.forward;
-    } else {
-      this.setState(() => {
-        return { mode: "enter", context: "sign_in" };
-      });
-    }
-  }
-
-  create() {
-    this.setState(() => {
-      return { mode: "create", context: "sign_up" };
-    });
-  }
-
-  verify(isfinish) {
-    if (isfinish) {
-    }
-  }
-
-  toggleMode() {
-    const prev = this.state.context;
-    common.rotate(this.state.mode, this.modes, action => {
-      this.setState({
-        prev: prev,
-        prevHover: prev + "_hover"
-      });
-      this[action]();
-    });
-  }
-
-  inputId(e) {
-    this.setState({ username: e.target.value });
-  }
-
-  inputPass(e) {
-    this.setState({ password: e.target.value });
-  }
-
-  postRequest() {
-    const params = {
-      actionType: "enter",
-      username: this.state.username,
-      password: this.state.password
-    };
-    common.postRequest(params, this.execpath, this.done);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="menu">
-          <div className="menu_wrapper">
-            <span>Do You Chenge To Mode ? </span>
-            <div
-              id="switcher"
-              className={this.state.prevHover + " switch_txt"}
-              onClick={this.toggleMode.bind(this)}
-            >
-              {this.state.prev.toUpperCase()}
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <form name="form">
-            <div className="wrapper">
-              <div className="title">
-                <h1>{common.appName}</h1>
-              </div>
-              <div>
-                <input
-                  type="text"
-                  name="username"
-                  placeholder="yourname?"
-                  onChange={this.inputId.bind(this)}
-                />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="password?"
-                  onChange={this.inputPass.bind(this)}
-                />
-              </div>
-              <div id="send">
-                <input
-                  className={this.state.context}
-                  type="text"
-                  value={this.state.context.toUpperCase()}
-                  readonly
-                  onClick={this.postRequest.bind(this)}
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
-
-const app = document.getElementById("app");
-ReactDOM.render(<Account />, app);
+render(
+  <Provider store={store}>
+    <Account />
+  </Provider>,
+  document.getElementById("app")
+);
