b52272b040c57d20d050e23ce54f28c801f63133
ryotakoyama <kryota.gin@gmail.com>
2020-01-12 19:23:00 +0900

    selectorsにアカウント作成時必須stateを定義

--- src/views/selectors/index.js
+++ src/views/selectors/index.js
@@ -4,3 +4,11 @@ export const selectFormdataForEnter = state => ({
   username: state.account.username,
   password: state.account.password
 });
+export const selectFormdataForVerify = state => ({
+  actionType: "verify",
+  username: state.account.username,
+  password: state.account.password,
+  bucket: state.account.bucket,
+  bucketkey: state.account.bucketkey,
+  bucketval: state.account.bucketval
+});
