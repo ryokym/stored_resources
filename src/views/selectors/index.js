5208b48ad65749d6f63f0065144ea8193fbca26f
ryotakoyama <kryota.gin@gmail.com>
2020-01-13 13:52:42 +0900

    selector, マストパラメータは共通化

--- src/views/selectors/index.js
+++ src/views/selectors/index.js
@@ -1,14 +1,19 @@
 export const selectMode = state => state.account.mode;
-export const selectFormdataForEnter = state => ({
-  actionType: "enter",
+export const selectFormdataForSignInOrUp = state => ({
+  actionType: state.account.mode,
   username: state.account.username,
   password: state.account.password
 });
-export const selectFormdataForVerify = state => ({
-  actionType: "verify",
-  username: state.account.username,
-  password: state.account.password,
+
+const formdataForCreateAccount = state => ({
   bucket: state.account.bucket,
   bucketkey: state.account.bucketkey,
   bucketval: state.account.bucketval
 });
+
+export const selectFormdataForCreateAccount = state => {
+  return Object.assign(
+    selectFormdataForSignInOrUp(state),
+    formdataForCreateAccount(state)
+  );
+};
