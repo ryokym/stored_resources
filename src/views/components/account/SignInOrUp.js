4b689958c94dd66b74cdc9dd185d255ed2ff886d
ryotakoyama <kryota.gin@gmail.com>
2020-01-15 21:08:16 +0900

    divタグは全てcomponentへ

--- src/views/components/account/SignInOrUp.js
+++ src/views/components/account/SignInOrUp.js
@@ -10,35 +10,39 @@ const inputPasswordCallback = (callback, e) => {
 
 const SignInOrUpComponent = props => {
   return (
-    <div className="wrapper">
-      <div className="title">
-        <h1>{common.appName}</h1>
-      </div>
-      <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="yourname?"
-          onChange={e => inputUserNameCallback(props.inputUserName, e)}
-          value={props.username}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password?"
-          onChange={e => inputPasswordCallback(props.inputPassword, e)}
-          value={props.password}
-        />
-      </div>
-      <div id="send">
-        <input
-          className={props.context}
-          type="text"
-          value={props.context.toUpperCase()}
-          readonly
-          onClick={() => props.requestPost()}
-        />
-      </div>
+    <div className="container">
+      <form name="form">
+        <div className="wrapper">
+          <div className="title">
+            <h1>{common.appName}</h1>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="username"
+              placeholder="yourname?"
+              onChange={e => inputUserNameCallback(props.inputUserName, e)}
+              value={props.username}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="password?"
+              onChange={e => inputPasswordCallback(props.inputPassword, e)}
+              value={props.password}
+            />
+          </div>
+          <div id="send">
+            <input
+              className={props.context}
+              type="text"
+              value={props.context.toUpperCase()}
+              readonly
+              onClick={() => props.requestPost()}
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
