74adc051d0ac04df7df63f6f2a12028a26a1aaff
ryotakoyama <kryota.gin@gmail.com>
2020-01-17 00:25:23 +0900

    main用、動作モードを返すreducer作成

--- src/views/reducers/index.js
+++ src/views/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from "redux";
 import modeReducer from "./account/modeReducer";
 import formReducer from "./account/formReducer";
+import behaviorReducer from "./main/behaviorReducer";
 
 export const accountReducer = combineReducers({
   modeReducer,
   formReducer
+});
+
+export const mainReducer = combineReducers({
+  behaviorReducer
 });
---
+++ src/views/reducers/main/behaviorReducer.js
@@ -0,0 +1,11 @@
+const initialAppState = {
+  mode: "change"
+};
+
+const behaviorReducer = (state = initialAppState, action) => {
+  return {
+    ...state
+  };
+};
+
+export default behaviorReducer;
