9628352b686a12d8d319d9b76ed99d6921958c72
ryotakoyama <kryota.gin@gmail.com>
2020-01-18 01:08:24 +0900

    propsは各componentで展開する

--- src/views/containers/Account.js
+++ src/views/containers/Account.js
@@ -3,44 +3,18 @@ import ReactDOM from "react-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../actions/accountAction";
+import Body from "../components/account/Body";
 import Menu from "../components/account/Menu";
-import SignInOrUp from "../components/account/SignInOrUp";
-import CreateAccount from "../components/account/CreateAccount";
 
-const view = (modeState, formState, actions) => {
-  return modeState.mode === "verify" ? (
-    <CreateAccount
-      mode={modeState.mode}
-      inputBucket={actions.inputBucket}
-      inputBucketVal={actions.inputBucketVal}
-      bucketkey={modeState.bucketkey}
-      closeModal={actions.closeModal}
-      requestPost={actions.requestPost}
-    />
-  ) : (
-    <SignInOrUp
-      context={modeState.context}
-      inputUserName={actions.inputUserName}
-      inputPassword={actions.inputPassword}
-      username={formState.username}
-      password={formState.password}
-      requestPost={actions.requestPost}
-    />
-  );
-};
 class Account extends React.Component {
   render() {
     const { formState, modeState, actions } = this.props;
     return (
       <>
-        <Menu
-          mode={modeState.mode}
-          context={modeState.context}
-          prevHover={modeState.prevHover}
-          toggleMode={actions.toggleMode}
-          prev={modeState.prev}
-        />
-        {view(modeState, formState, actions)}
+        <Menu modeState={modeState} actions={actions} />
+        <Body formState={formState} modeState={modeState} actions={actions}>
+          {this.props.children}
+        </Body>
       </>
     );
   }
