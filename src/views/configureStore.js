ed587baf3fbeb08c857af1476da6edbbb77507e3
ryotakoyama <kryota.gin@gmail.com>
2020-01-18 14:04:19 +0900

    sagas/main/index.jsをエントリとしてmainにredux-sagaを介入させる

--- src/views/configureStore.js
+++ src/views/configureStore.js
@@ -1,19 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { accountReducer, mainReducer } from "./reducers";
-import rootSaga from "./sagas/accountSaga";
+import accountRootSaga from "./sagas/accountSaga";
+import mainRootSaga from "./sagas/main/index";
 
 export function accountConfigureStore() {
   const sagaMiddleware = createSagaMiddleware();
   const store = createStore(accountReducer, applyMiddleware(sagaMiddleware));
-  sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(accountRootSaga);
 
   return store;
 }
 export function mainConfigureStore() {
   const sagaMiddleware = createSagaMiddleware();
   const store = createStore(mainReducer, applyMiddleware(sagaMiddleware));
-  sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(mainRootSaga);
 
   return store;
 }
