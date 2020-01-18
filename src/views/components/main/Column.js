0e0eb745680bbd889a9395bdb3a44276a7ea579c
ryotakoyama <kryota.gin@gmail.com>
2020-01-18 14:07:25 +0900

    componentの構造修正。複数columnの中に複数rowの予定

--- src/views/components/main/Column.js
+++ src/views/components/main/Column.js
@@ -5,7 +5,14 @@ const ColumnComponent = ({ ...props }) => {
   const { behaviorState, actions } = props;
   return (
     <div id="column">
-      <div className="level" data-level="1" data-dir="">
+      <div className="level">
+        <div className="row" data-hierarchy="0">
+          <img src="/assets/img/add.svg" className="open enable" />
+          <img src="/assets/img/close.svg" className="close" />
+        </div>
+        <div className="row createNewDirRow">
+          <input className="textbox" type="text" />
+        </div>
         <Row behaviorState={behaviorState} actions={actions} />
       </div>
     </div>
--- src/views/components/main/Row.js
+++ src/views/components/main/Row.js
@@ -5,16 +5,8 @@ const RowComponent = ({ ...props }) => {
   console.log(behaviorState);
   return (
     <>
-      <div className="row">
-        <img src="/assets/img/add.svg" className="open enable" />
-        <img src="/assets/img/close.svg" className="close" />
-        {behaviorState.behavior}
-      </div>
-      <div className="row createNewDirRow">
-        <input className="textbox" type="text" />
-      </div>
-      <div className="row">
-        <p className="row_item"></p>
+      <div className="row" data-hierarchy="0" data-path="">
+        <p className="row_item">{behaviorState.behavior}</p>
       </div>
     </>
   );
