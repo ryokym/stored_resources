411c64e733521b94263c50c7648f7a3bccff0871
ryotakoyama <kryota.gin@gmail.com>
2020-01-20 01:53:20 +0900

    structureをmapオブジェクトとする。structureState用reducerに追記

--- src/views/reducers/main/structureReducer.js
+++ src/views/reducers/main/structureReducer.js
@@ -1,12 +1,18 @@
 const initialAppState = {
-  structure: []
+  structure: new Map(),
+  workdir: ""
 };
 
 const structureReducer = (state = initialAppState, action) => {
-  if (action.type === "GET_RESOURCES") {
+  if (action.type === "GET_STRUCTURE") {
     return {
       ...state,
-      structure: action.payload.resources
+      structure: action.payload.structure
+    };
+  } else if (action.type === "PRINT_WORKING_DIRECTORY") {
+    return {
+      ...state,
+      workdir: action.payload.workdir
     };
   } else {
     return {
