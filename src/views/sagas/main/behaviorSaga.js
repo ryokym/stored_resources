fdce07a9b190a96e7b33fc817efbf5ff2e770665
ryotakoyama <kryota.gin@gmail.com>
2020-01-20 02:02:43 +0900

    bucket構造の上下移動をGUIで行うロジックを追加

--- src/views/sagas/main/behaviorSaga.js
+++ src/views/sagas/main/behaviorSaga.js
@@ -1,6 +1,11 @@
-import { select, call, fork, takeLatest, put } from "redux-saga/effects";
+import { select, call, fork, takeLatest, put, join } from "redux-saga/effects";
 import common from "../../utils/common";
-import { getResources } from "../../actions/mainAction";
+import { rebuildWorkdir, rebuildStructure } from "./modules/generator";
+import {
+  selectStructure,
+  selectWorkingDirectory
+} from "../../selectors/mainSelector";
+import { getStructure, printWorkingDirectory } from "../../actions/mainAction";
 
 function doAsync(params) {
   const pathto = "/api/main.php";
@@ -11,9 +16,35 @@ function* fetchListBucket() {
   const params = { actionType: "initiate" };
   const response = yield call(doAsync, params);
   const resources = JSON.parse(response);
-  yield put(getResources("change", resources));
+  let structure = yield select(selectStructure);
+  structure.set(structure.size, resources);
+  yield put(getStructure("change", structure));
 }
 
-const behaviorSaga = [takeLatest("INITIATE", fetchListBucket)];
+function* fetchResources(props) {
+  let params = props.payload;
+  let workdir = yield select(selectWorkingDirectory);
+  workdir = yield call(rebuildWorkdir, workdir, params.hierarchy);
+  params.actionType = "change";
+  params.path = workdir;
+  const response = yield call(doAsync, params);
+  const resources = JSON.parse(response).result;
+  const structure = yield select(selectStructure);
+  const newStructure = yield call(
+    rebuildStructure,
+    structure,
+    params.hierarchy
+  );
+  newStructure.set(newStructure.size, resources);
+  yield put(getStructure("change", newStructure));
+  const nextWorkdir =
+    workdir !== "" ? [workdir, params.name].join("/") : params.name;
+  yield put(printWorkingDirectory(nextWorkdir));
+}
+
+const behaviorSaga = [
+  takeLatest("INITIATE", fetchListBucket),
+  takeLatest("CLICK_ROW", fetchResources)
+];
 
 export default behaviorSaga;
