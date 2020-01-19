94be66bb5db1f3aabfdf57c0e8dd8eb585fc0704
ryotakoyama <kryota.gin@gmail.com>
2020-01-20 02:01:21 +0900

    column内のrow数分だけ描画。sagaに渡るオブジェクトは各要素の引数に持たせる

--- src/views/components/main/Row.js
+++ src/views/components/main/Row.js
@@ -1,20 +1,31 @@
 import React from "react";
 
-const renderRows = items => {
-  const result = items.map(item => {
-    return (
-      <>
-        <div className="row" data-hierarchy="0" data-path="">
-          <p className="row_item">{item}</p>
-        </div>
-      </>
-    );
-  });
-  return result;
-};
-const RowComponent = structure => {
-  const items = Object.values(structure)[0];
-  return items.length !== 0 && renderRows(items);
-};
+class RowComponent extends React.Component {
+  renderRows = items => {
+    const result = items.map(item => {
+      return (
+        <>
+          <div
+            className="row"
+            data-hierarchy={this.props.hierarchy}
+            onClick={e =>
+              this.props.click_row({
+                name: e.target.innerText,
+                hierarchy: e.target.getAttribute("data-hierarchy")
+              })
+            }
+          >
+            {item}
+          </div>
+        </>
+      );
+    });
+    return result;
+  };
+  render() {
+    const items = this.props.structure || [];
+    return items.length !== 0 && this.renderRows(items);
+  }
+}
 
 export default RowComponent;
