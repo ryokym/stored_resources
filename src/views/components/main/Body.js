a129880cd3face6e836c96d04bf9ce4bd499422f
ryotakoyama <kryota.gin@gmail.com>
2020-01-20 01:55:29 +0900

    BodyからStructureのcolumn数分だけColumnComponent描画

--- src/views/components/main/Body.js
+++ src/views/components/main/Body.js
@@ -1,34 +1,50 @@
 import React from "react";
 import Column from "./Column";
 
-const BodyComponent = ({ ...props }) => {
-  const { behaviorState, structureState, actions } = props;
-  return (
-    <div className="container">
-      <Column
-        behaviorState={behaviorState}
-        structureState={structureState}
-        actions={actions}
-      >
-        {props.children}
-      </Column>
-      <div id="display">
-        <div id="toggles">
-          <div id="upload_area">
-            <img id="upload_drop_area" src="/assets/img/dragDrop.svg" />
-          </div>
-          <pre id="preview">
-            <code className="prettyprint"></code>
-          </pre>
-          <div id="remove_area">
-            <div id="remove_drop_area">
-              <img src="/assets/img/trush.png" />
+class BodyComponent extends React.Component {
+  renderColumns = props => {
+    const { behaviorState, structureState, actions } = props;
+    if (structureState.structure.size > 0) {
+      const size = structureState.structure.size;
+      const sizes = [...Array(size).keys()];
+      const result = sizes.map(index => {
+        return (
+          <Column
+            behaviorState={behaviorState}
+            structureState={structureState}
+            index={index}
+            actions={actions}
+          >
+            {this.props.children}
+          </Column>
+        );
+      });
+      return result;
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        {this.renderColumns(this.props)}
+        <div id="display">
+          <div id="toggles">
+            <div id="upload_area">
+              <img id="upload_drop_area" src="/assets/img/dragDrop.svg" />
+            </div>
+            <pre id="preview">
+              <code className="prettyprint"></code>
+            </pre>
+            <div id="remove_area">
+              <div id="remove_drop_area">
+                <img src="/assets/img/trush.png" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default BodyComponent;
