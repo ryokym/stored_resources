4833203dfe03ca9e711fc2a9932801dde4a678fe
ryotakoyama <kryota.gin@gmail.com>
2020-01-21 00:08:34 +0900

    ファイル削除描画領域をComponent化

---
+++ src/views/components/main/fields/RemoveField.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RemoveFieldComponent = () => {
+  return (
+    <div id="remove_area">
+      <div id="remove_drop_area">
+        <img src="/assets/img/trush.png" />
+      </div>
+    </div>
+  );
+};
+
+export default RemoveFieldComponent;
