cd31764ec4f2f315676cebace3ad9c00f14291de
ryotakoyama <kryota.gin@gmail.com>
2020-01-21 00:08:06 +0900

    ファイルアップロード描画用Component追加

---
+++ src/views/components/main/fields/UploadField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UploadFieldComponent = () => {
+  return (
+    <div id="upload_area">
+      <img id="upload_drop_area" src="/assets/img/dragDrop.svg" />
+    </div>
+  );
+};
+
+export default UploadFieldComponent;
