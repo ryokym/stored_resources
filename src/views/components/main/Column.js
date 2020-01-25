2bf6e3f5d163c3486b6fc07abcb62bb1120136a0
ryotakoyama <kryota.gin@gmail.com>
2020-01-26 02:07:26 +0900

    mkdirのdomを除去。modalにするので必要ない

--- src/views/components/main/Column.js
+++ src/views/components/main/Column.js
@@ -6,13 +6,6 @@ const ColumnComponent = ({ ...props }) => {
   return (
     <div id="column">
       <div className="level">
-        <div className="row" data-hierarchy="0">
-          <img src="/assets/img/add.svg" className="open enable" />
-          <img src="/assets/img/close.svg" className="close" />
-        </div>
-        <div className="row createNewDirRow">
-          <input className="textbox" type="text" />
-        </div>
         <Row
           structure={structureState.structure.get(index)}
           hierarchy={index}
