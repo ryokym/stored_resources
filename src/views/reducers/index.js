0ccab170dd4ced50e72d3dbb72b058c2cdb76995
ryotakoyama <kryota.gin@gmail.com>
2020-01-27 00:15:58 +0900

    modalのstate管理用のreducerをaccount, main双方にcombine

--- src/views/reducers/index.js
+++ src/views/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import modalReducer from "./modalReducer";
 import modeReducer from "./account/modeReducer";
 import formReducer from "./account/formReducer";
 import behaviorReducer from "./main/behaviorReducer";
@@ -7,11 +8,13 @@ import fieldReducer from "./main/fieldReducer";
 import modalFormReducer from "./main/modalFormReducer";
 
 export const accountReducer = combineReducers({
+  modalReducer,
   modeReducer,
   formReducer
 });
 
 export const mainReducer = combineReducers({
+  modalReducer,
   behaviorReducer,
   structureReducer,
   fieldReducer,
