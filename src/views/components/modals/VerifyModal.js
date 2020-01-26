60a46d1a814c192359d5cccac5bbe746ce85186a
ryotakoyama <kryota.gin@gmail.com>
2020-01-27 00:09:19 +0900

    verify用modalComponentをReact-modalに変更

---
+++ src/views/components/modals/VerifyModal.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Modal from "react-modal";
+
+const VerifyModalComponent = props => {
+  const { modalState, formState, actions } = props;
+  return (
+    <Modal
+      isOpen={modalState.modalVerifyIsOpen}
+      className="verify_modal_content"
+      overlayClassName="modal_overlay"
+      onRequestClose={actions.clickCloseModal}
+      onAfterClose={() => actions.requireSigninOrUpForm()}
+    >
+      <div className="verify_modal_content_wrapper">
+        <p>Direction...</p>
+        <p>GENERATED KEY</p>
+        <p>{formState.bucketkey}</p>
+        <p>
+          <ol>Access 3s bucket from aws management console.</ol>
+          <ol>Then select "Tag" in property settings..</ol>
+          <ol>Set the above key and set arbitrary value.</ol>
+          <ol>Finally, enter the same value in the input field below.</ol>
+        </p>
+      </div>
+      <input
+        type="text"
+        name="bucket"
+        placeholder="your S3bucketname?"
+        onChange={e => actions.inputBucket(e.target.value)}
+      />
+      <input
+        type="password"
+        name="bucketval"
+        placeholder="set value"
+        onChange={e => actions.inputBucketVal(e.target.value)}
+      />
+      <div className="verify_modal_content_btns">
+        <div onClick={() => actions.requestPost()}>VERIFY</div>
+        <div onClick={() => actions.clickCloseModal()}>DISMISS</div>
+      </div>
+    </Modal>
+  );
+};
+export default VerifyModalComponent;
