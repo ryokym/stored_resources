68699d882acb1ebd685cb6001b5422c996adbd18
ryotakoyama <kryota.gin@gmail.com>
2020-01-28 23:25:22 +0900

    コメント削除

--- src/views/components/account/Body.js
+++ src/views/components/account/Body.js
@@ -1,15 +1,6 @@
 import React from "react";
 import Form from "./Form";
 
-// const renderFormOrModal = props => {
-//   const { modeState, formState, actions } = props;
-//   return modeState.mode === "verify" ? (
-//     <VerifyModal formState={formState} actions={actions} />
-//   ) : (
-//     <Form modeState={modeState} formState={formState} actions={actions} />
-//   );
-// };
-
 const BodyComponent = props => {
   return (
     <div className="container">
