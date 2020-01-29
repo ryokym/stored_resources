a45b45408d46550398ab5ebfbd02593f9cde6c09
ryotakoyama <kryota.gin@gmail.com>
2020-01-30 00:28:18 +0900

    behaviorをturnするUIロジックを追加

--- src/views/containers/Account.js
+++ src/views/containers/Account.js
@@ -3,16 +3,48 @@ import ReactDOM from "react-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { accountActions } from "../actions";
+import common from "../utils/common";
 import Body from "../components/account/Body";
 import Menu from "../components/account/Menu";
 
 class Account extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getInvertedContext = this.getInvertedContext.bind(this);
+    this.mapedContexts = new Map([
+      ["enter", "sign_in"],
+      ["create", "sign_up"]
+    ]);
+  }
+
+  getInvertedContext(behavior) {
+    if (behavior === "verify") return { ...this.accountProps };
+    const [anotherBehavior, anotherContext] = common.rotate(
+      behavior,
+      this.mapedContexts
+    );
+    return {
+      anotherBehavior: anotherBehavior,
+      anotherContext: anotherContext,
+      context: this.mapedContexts.get(behavior),
+      contextHover: `${anotherContext}_hover`
+    };
+  }
+
   render() {
     const { behaviorState, actions } = this.props;
+    const { ...accountProps } = this.getInvertedContext(behaviorState.behavior);
+    this.accountProps = { ...accountProps };
     return (
       <>
-        <Menu behaviorState={behaviorState} actions={actions} />
-        <Body {...this.props}>{this.props.children}</Body>
+        <Menu
+          {...accountProps}
+          behaviorState={behaviorState}
+          actions={actions}
+        />
+        <Body {...this.props} {...accountProps}>
+          {this.props.children}
+        </Body>
       </>
     );
   }
