b4a7a299b46d3089b935926fe0ff2c71b7e1f4d8
ryotakoyama <kryota.gin@gmail.com>
2020-01-29 22:44:17 +0900

    レンダリング単位で分割していたsagaの構造を変更するため一旦削除

--- src/views/sagas/main/index.js
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects";
-import logoutSaga from "./logoutSaga";
-import behaviorSaga from "./behaviorSaga";
-
-export default function* rootSaga() {
-  yield all([...logoutSaga, ...behaviorSaga]);
-}
