09d383a44897894458343cfb82bff8387fd91e59
ryotakoyama <kryota.gin@gmail.com>
2020-01-31 01:18:45 +0900

    新規ディレクトリ作成の非同期ロジックをsagaに追加

--- src/views/sagas/makeDirectorySaga.js
+++ src/views/sagas/makeDirectorySaga.js
@@ -1,4 +1,6 @@
 import { select, call, takeLatest, put } from "redux-saga/effects";
+import { selectStructure } from "../selectors/mainSelector";
+import { mainActions } from "../actions";
 import common from "../utils/common";
 
 function doAsync(params) {
@@ -7,7 +9,18 @@ function doAsync(params) {
 }
 
 function* fetchMakeDirectory(props) {
-  console.log(props);
+  let params = props.payload;
+  const isCorrectName = common.validateFileFormat(params.name);
+  if (isCorrectName) {
+    params.actionType = "makedir";
+    yield call(doAsync, params);
+    let structure = yield select(selectStructure);
+    const hierarchy = common.getHierarchy(params.path);
+    let column = structure.get(hierarchy);
+    column.push(params.name);
+    structure.set(hierarchy, column);
+    yield put(mainActions.continuousInputMkdirForm());
+    yield put(mainActions.redrawStructure(structure));
+  }
 }
-
 export default takeLatest("MAKE_DIRECTORY", fetchMakeDirectory);
