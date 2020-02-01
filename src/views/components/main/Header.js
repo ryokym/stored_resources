fcca43ed6bc6e912e200a3359981fe2a7c547794
ryotakoyama <kryota.gin@gmail.com>
2020-02-01 21:02:24 +0900

    特定のアイコンクリック時に他の操作を制御する。removeの際には他のアイコンは全てdisabled

--- src/views/components/main/Header.js
+++ src/views/components/main/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Common from "../../utils/common";
+import Common from "~/utils/common";
 
 class HeaderComponent extends React.Component {
   constructor(props) {
@@ -16,14 +16,25 @@ class HeaderComponent extends React.Component {
     ["logout.svg", "clickLogout"]
   ];
 
-  renderMenuitems(list, actions) {
+  disabledIfNeeded(action, fieldState) {
+    if (action !== "clickRemove" && fieldState.isremove === true) {
+      return "disabledIcon";
+    } else {
+      return "";
+    }
+  }
+
+  renderMenuitems(list, actions, fieldState) {
     const result = list.map(ary => {
       const [material, action] = ary;
       return (
         <div className="menu_item">
           <img
             onClick={typeof actions[action] === "function" && actions[action]}
-            className="menu_items menu_item_icon"
+            className={`menu_items menu_item_icon ${this.disabledIfNeeded(
+              action,
+              fieldState
+            )}`}
             src={`/assets/img/${material}`}
           />
         </div>
@@ -33,13 +44,14 @@ class HeaderComponent extends React.Component {
   }
 
   render() {
+    const { actions, fieldState } = this.props;
     return (
       <div className="header">
         <div id="title" contenteditable="true">
           {Common.appName}
         </div>
         <div className="menu">
-          {this.renderMenuitems(this.mapMaterialToAction, this.props.actions)}
+          {this.renderMenuitems(this.mapMaterialToAction, actions, fieldState)}
         </div>
       </div>
     );
