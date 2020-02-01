64043a45884f63f826ec9cec3811678ff392e194
ryotakoyama <kryota.gin@gmail.com>
2020-02-01 21:08:03 +0900

    clickRemoveアクションで発動したsagaからstate取得用のselector追加

--- src/views/selectors/mainSelector.js
+++ src/views/selectors/mainSelector.js
@@ -1,2 +1,3 @@
 export const selectStructure = state => state.structureReducer.structure;
 export const selectWorkingDirectory = state => state.structureReducer.workdir;
+export const selectFieldStatusForRemove = state => state.fieldReducer.isremove;
