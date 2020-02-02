a3d49dcd189fb7ac5e714019043787fd37d5707f
ryotakoyama <kryota.gin@gmail.com>
2020-02-02 13:49:21 +0900

    toastは通常Actionで発行。stateで判定が必要な場合のみsagaで発行。

--- src/views/components/main/fields/RemoveField.js
+++ src/views/components/main/fields/RemoveField.js
@@ -1,10 +1,6 @@
 import React from "react";
-import baseToast from "~/configureToast";
-import { toast } from "react-toastify";
 
 const RemoveFieldComponent = () => {
-  toast.configure(baseToast);
-  toast.error("mode remove");
   return <div id="remove_area">Click on the resource you want to delete</div>;
 };
 
--- src/views/sagas/clickRemoveSaga.js
+++ src/views/sagas/clickRemoveSaga.js
@@ -1,10 +1,14 @@
 import { select, takeLatest, put } from "redux-saga/effects";
 import { selectFieldStatusForRemove } from "../selectors/mainSelector";
 import { mainActions } from "../actions";
+import baseToast from "~/configureToast";
+import { toast } from "react-toastify";
+toast.configure(baseToast);
 
 function* toggleRemove() {
   let isremove = yield select(selectFieldStatusForRemove);
-  const behavior = isremove ? "remove" : "change";
+  const behavior = isremove ? "change" : "remove";
+  behavior === "remove" && toast.error("mode remove");
   yield put(mainActions.clickRemoveNext(behavior, !isremove));
 }
 
