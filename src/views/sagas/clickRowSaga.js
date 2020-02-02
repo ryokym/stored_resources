f8e0ac4c141bf73170573e5c5a0733aa5c16a732
ryotakoyama <kryota.gin@gmail.com>
2020-02-02 13:56:29 +0900

    clickRow時にbehaviorがremoveだったらremove関連のaction発行

--- src/views/sagas/clickRowSaga.js
+++ src/views/sagas/clickRowSaga.js
@@ -2,6 +2,7 @@ import { select, call, takeLatest, put } from "redux-saga/effects";
 import common from "../utils/common";
 import { rebuildWorkdir, rebuildStructure } from "./modules/generator";
 import {
+  selectBehavior,
   selectStructure,
   selectWorkingDirectory
 } from "../selectors/mainSelector";
@@ -12,31 +13,43 @@ function doAsync(params) {
   return common.callFetch(params, pathto);
 }
 
-function* fetchResources(props) {
+function* dispatchActionAtBehavior(props) {
   let params = props.payload;
+  const behavior = yield select(selectBehavior);
   let workdir = yield select(selectWorkingDirectory);
+  params.actionType = behavior;
 
-  workdir = yield call(rebuildWorkdir, workdir, params.hierarchy);
-  const isRootdir = workdir === "" ? true : false;
-  const newWorkdir = isRootdir ? params.name : [workdir, params.name].join("/");
-  yield put(mainActions.printWorkingDirectory(newWorkdir));
-  params.actionType = "change";
-  params.path = workdir;
-  let response = yield call(doAsync, params);
-  response = JSON.parse(response);
-  const structure = yield select(selectStructure);
-  const newStructure = yield call(
-    rebuildStructure,
-    structure,
-    params.hierarchy
-  );
-  if (response.isFile === false) {
-    yield put(mainActions.clickDirectoryResource());
-    newStructure.set(newStructure.size, response.result);
-  } else {
-    yield put(mainActions.getFileContent(response.result));
+  if (params.actionType === "change") {
+    workdir = yield call(rebuildWorkdir, workdir, params.hierarchy);
+    const isRootdir = workdir === "" ? true : false;
+    const newWorkdir = isRootdir
+      ? params.name
+      : [workdir, params.name].join("/");
+    yield put(mainActions.printWorkingDirectory(newWorkdir));
+    params.path = workdir;
+    let response = yield call(doAsync, params);
+    response = JSON.parse(response);
+    const structure = yield select(selectStructure);
+    const newStructure = yield call(
+      rebuildStructure,
+      structure,
+      params.hierarchy
+    );
+    if (response.isFile === false) {
+      yield put(mainActions.clickDirectoryResource());
+      newStructure.set(newStructure.size, response.result);
+    } else {
+      yield put(mainActions.getFileContent(response.result));
+    }
+    yield put(mainActions.getNewStructure("change", newStructure));
+  } else if (params.actionType === "remove") {
+    params.path = common.rebuildPathForSpecifiedHierarchy(
+      workdir,
+      params.hierarchy
+    );
+    yield put(mainActions.requireRemoveModal(params.name, params.path));
+    yield put(mainActions.clickOpenModalRemove());
   }
-  yield put(mainActions.getNewStructure("change", newStructure));
 }
 
-export default takeLatest("CLICK_ROW", fetchResources);
+export default takeLatest("CLICK_ROW", dispatchActionAtBehavior);
