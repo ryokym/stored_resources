585166da44f9b4a897c785a752ecfbca93820e5f
ryotakoyama <kryota.gin@gmail.com>
2020-02-07 23:56:23 +0900

    selectorsをまとめた

--- src/views/selectors/mainSelector.js
+++
@@ -1,4 +0,0 @@
-export const selectBehavior = state => state.behaviorReducer.behavior;
-export const selectStructure = state => state.structureReducer.structure;
-export const selectWorkingDirectory = state => state.structureReducer.workdir;
-export const selectFieldState = state => state.fieldReducer;
---
+++ src/views/utils/selectors.js
@@ -0,0 +1,4 @@
+export const selectBehavior = state => state.behaviorReducer;
+export const selectStructure = state => state.structureReducer;
+export const selectFieldState = state => state.fieldReducer;
+export const selectForm = state => state.formReducer;
